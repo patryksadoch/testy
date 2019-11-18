2fbc5cc3686635f85d3fe522ef52c9eb2e9f2c0c
Patryk Sadoch <petrriksad@gmail.com>
2019-11-18 20:07:31 +0100

    aaa

--- main_R.R
+++ main_R.R
@@ -1,8 +1,24 @@
 ###
 print("hello")
 
+<<<<<<< HEAD
 num_der <- function(f,x,H){
   f(x+h) - f(x)/H
 
   
   }
+=======
+num_der <- function(f,x,h){
+  f(x+h) - f(x)/h
+}
+
+f<- function(x) exp (x)
+
+num_der(f,3,1/100000)
+
+
+n<- 200
+samp <- rt(n,10)
+print (samp)
+plot(samp)
+>>>>>>> new-features
